fdd4bfa2aac481365f93b59a6efc40196ef2fd79
Yang Luo <hsluoyz@qq.com>
2020-04-22 21:19:42 +0800

    Add Baidu analytics.

---
+++ website/static/js/baidu-analytics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?85c2631f5ef83d6284a8f09d2d5d9a2b";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();

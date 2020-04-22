bbd9d61535d23fb4284ad2ff355ff148c9872486
Yang Luo <hsluoyz@qq.com>
2020-04-22 11:35:50 +0800

    Update the site info.

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,9 +21,9 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site' /* title for your website */,
-  tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com' /* your website url */,
+  title: 'Mouselog' /* title for your website */,
+  tagline: 'A web-based tool for logging, visualizing and analyzing mouse events like movement and clicks',
+  url: 'https://docs.mouselog.org' /* your website url */,
   baseUrl: '/' /* base url for your project */,
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
@@ -31,7 +31,7 @@ const siteConfig = {
 
   // Used for publishing and more
   projectName: 'test-site',
-  organizationName: 'facebook',
+  organizationName: 'microsoft',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
